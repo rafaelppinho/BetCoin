132af79fb50fc641678e5f3ded3da0ff0857473a
NicolasLepore <nicolaslpr06@gmail.com>
2025-06-01 19:36:40 -0300

    Novos formularios e inicio do CSS

---
+++ Frontend/src/app/button.js
@@ -0,0 +1,14 @@
+const btnViewPassword = document.getElementById("btn-view-password");
+const inputPassword = document.getElementById("password");
+const image = btnViewPassword.children[0];
+
+btnViewPassword.addEventListener("click", (e) => {
+    if(inputPassword.type == "password") {
+        inputPassword.type = "text";
+        image.src = "/public/images/mostrar-senha.png";
+        
+    } else {
+        inputPassword.type = "password";
+        image.src = "/public/images/senha-escondida.png";
+    }
+})
