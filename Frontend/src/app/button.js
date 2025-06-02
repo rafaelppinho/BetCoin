4cee98773cdbf21991a4de2d5e7e02107c06477e
NicolasLepore <nicolaslpr06@gmail.com>
2025-06-01 22:04:31 -0300

    Pagina de Clientes completa e estilizada

--- Frontend/src/app/button.js
+++ Frontend/src/app/button.js
@@ -5,10 +5,10 @@ const image = btnViewPassword.children[0];
 btnViewPassword.addEventListener("click", (e) => {
     if(inputPassword.type == "password") {
         inputPassword.type = "text";
-        image.src = "/public/images/mostrar-senha.png";
+        image.src = "../../public/images/mostrar-senha.png";
         
     } else {
         inputPassword.type = "password";
-        image.src = "/public/images/senha-escondida.png";
+        image.src = "../../public/images/senha-escondida.png";
     }
 })
