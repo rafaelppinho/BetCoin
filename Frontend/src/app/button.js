dd5bbb8240eae562f93908abfe616de5f38cd176
SpaceGProductions <spacegproductions@gmail.com>
2025-06-02 00:44:27 -0300

    pagina de jogos finalizada

--- Frontend/src/app/button.js
+++ Frontend/src/app/button.js
@@ -1,14 +1,8 @@
 const btnViewPassword = document.getElementById("btn-view-password");
 const inputPassword = document.getElementById("password");
 const image = btnViewPassword.children[0];
-const selected = document.getElementById("TypeJogos");
 
 btnViewPassword.addEventListener("click", (e) => {
-    if (selected.value === "default") {
-        e.preventDefault();
-        alert("Por favor, selecione um tipo de jogo.");
-        return;
-    }
     if(inputPassword.type == "password") {
         inputPassword.type = "text";
         image.src = "../../public/images/mostrar-senha.png";
